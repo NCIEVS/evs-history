3002ff1b2871a799920ecffe9dec7b9b23f71e63
Bob Dionne <dionne@dionne-associates.com>
2018-03-17 15:36:11 -0400

    Tweaks

--- src/main/java/gov/nih/nci/evs/EVSHistoryTab.java
+++ src/main/java/gov/nih/nci/evs/EVSHistoryTab.java
@@ -3,5 +3,10 @@ package gov.nih.nci.evs;
 import org.protege.editor.owl.ui.OWLWorkspaceViewsTab;
 
 public class EVSHistoryTab extends OWLWorkspaceViewsTab {
+	
+	@Override
+	public void initialise() {    	
+		super.initialise();
+	}
 
 }
